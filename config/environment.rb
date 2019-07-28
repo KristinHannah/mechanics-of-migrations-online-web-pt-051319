3c3caec329f23695b6cb4da982eba1d0831ed2e6
Kristin Kraemer <kristinhannahk@gmail.com>
2019-07-28 21:49:55 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -11,7 +11,7 @@ Bundler.require
 
 # put the code to connect to the database here
 ActiveRecod::Base.establish_connection(
-  :adapter => "sqlite3".
+  :adapter => "sqlite3",
   :database => "db/artists.sqlite"
   )
 
